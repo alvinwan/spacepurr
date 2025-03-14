5dbcf84e63e1059221e5841ffa96c47cf0c88b26
Alvin Wan <alvinwan@users.noreply.github.com>
2025-03-14 00:57:54 -0700

    added togglable switch

---
+++ Assets/SwitchController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class SwitchController : MonoBehaviour, IPointerClickHandler
+{
+    private bool isOn = false;
+
+    public RectTransform SwitchRect;
+
+    private Vector3 StartPosition;
+
+    public void TurnOn()
+    {
+        isOn = true;
+        Debug.Log("Switch is on");
+        SwitchRect.anchoredPosition = StartPosition + Vector3.up * 5;
+    }
+
+    public void TurnOff()
+    {
+        isOn = false;
+        Debug.Log("Switch is off");
+        SwitchRect.anchoredPosition = StartPosition;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Toggle();
+    }
+
+    void Toggle()
+    {
+        if (isOn)
+        {
+            TurnOff();
+        }
+        else
+        {
+            TurnOn();
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartPosition = SwitchRect.anchoredPosition;
+    }
+}
