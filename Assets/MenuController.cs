f5530dfceb6a642cd62f68dd1a48dff27444dd17
Alvin Wan <alvinwan@users.noreply.github.com>
2025-03-15 13:37:20 -0700

    nicer homepage

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MenuController : MonoBehaviour, IPointerClickHandler
+public class MenuController : MonoBehaviour
 {
     public LevelController Level;
     public void Start()
     {
         gameObject.SetActive(true);
     }
-    public void OnPointerClick(PointerEventData eventData)
+    public void OnStartClicked()
     {
         Level.OnLevelStart();
     }
