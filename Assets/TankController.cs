033b1b173eb58b62de6d8da218388e93117c5dd0
Alvin Wan <alvinwan@users.noreply.github.com>
2025-03-15 02:00:00 -0700

    remove glass cover

--- Assets/TankController.cs
+++ Assets/TankController.cs
@@ -3,21 +3,40 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using System.Collections;
 
-public class TankController : MonoBehaviour
+public class TankController : MonoBehaviour, IPointerDownHandler
 {
     // game objects
     public GameObject Water;
     public PumpController Pump;
     public CatController Cat;
     public TankCapController TankCap;
+    public GameObject TankCover;
+
+    // internal state
+    private bool isEmptied = false;
 
     void Start()
     {
         Pump.OnPumpReady += OnPumpReady;
     }
 
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!isEmptied)
+        {
+            return;
+        }
+        TankCover.SetActive(false);
+        TankCap.gameObject.SetActive(false);
+    }
+
     public void OnPumpReady(bool _)
     {
+        if (isEmptied)
+        {
+            return;
+        }
+        isEmptied = true;
         TankCap.Press();
         Water.SetActive(false);
         Cat.Awaken();
