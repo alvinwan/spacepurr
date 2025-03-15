6b3d99fa76c0bea35fffd84752e3851475651d94
Alvin Wan <alvinwan@users.noreply.github.com>
2025-03-15 01:39:04 -0700

    added locks

---
+++ Assets/LockController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class LockController : MonoBehaviour
+{
+    // game objects
+    public GameObject LockOn;
+    public GameObject LockOff;
+    public SwitchController Switch;
+
+    public void SetState(bool isLocked)
+    {
+        LockOn.SetActive(isLocked);
+        LockOff.SetActive(!isLocked);
+    }
+
+    public void OnSwitchToggle(bool isSwitchOn)
+    {
+        SetState(isSwitchOn);
+    }
+
+    public void Start()
+    {
+        SetState(false);
+        Switch.OnSwitchToggled += OnSwitchToggle;
+    }
+}
