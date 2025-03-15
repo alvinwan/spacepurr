7eb60dd0ddc75778a35c49950cf9fce815b562ad
Alvin Wan <alvinwan@users.noreply.github.com>
2025-03-15 15:08:12 -0700

    meow twice after waking up

--- Assets/CatController.cs
+++ Assets/CatController.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
+using System.Collections;
 
 public class CatController : MonoBehaviour
 {
     // internal state
     private Animator animator;
+    private AudioSource MeowSound;
 
     public void Start()
     {
         animator = GetComponent<Animator>();
+        MeowSound = GetComponent<AudioSource>();
     }
 
     public void Awaken()
     {
         animator.SetTrigger("awaken");
+        StartCoroutine(Meow());
+    }
+
+    IEnumerator Meow()
+    {
+        MeowSound.Play();
+        yield return new WaitForSeconds(5f);
+        MeowSound.Play();
     }
 }
