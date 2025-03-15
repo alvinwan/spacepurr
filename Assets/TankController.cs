58238d8ea500c333d0383306bc7e9e7283e411ab
Alvin Wan <alvinwan@users.noreply.github.com>
2025-03-15 00:26:34 -0700

    added water tank cap clickable, release after delay

---
+++ Assets/TankController.cs
@@ -0,0 +1,83 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class TankController : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
+{
+    // game objects
+    public GameObject TankCap;
+    public GameObject TankCapHighlighted;
+    public GameObject TankCapPressed;
+
+    // internal state
+    private bool isPressed = false;
+    private IEnumerator Coroutine;
+
+    void Start()
+    {
+        SetState(0);
+    }
+
+    void SetState(int state)
+    {
+        TankCap.SetActive(state == 0);
+        TankCapHighlighted.SetActive(state == 1);
+        TankCapPressed.SetActive(state == 2);
+    }
+
+    public void Press()
+    {
+        isPressed = true;
+        SetState(2);
+    }
+
+    public void Release()
+    {
+        isPressed = false;
+        SetState(0);
+    }
+
+    IEnumerator ReleaseAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Release();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        SetState(1);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!isPressed)
+        {
+            SetState(0);
+        }
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        SetState(1);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        if (!isPressed)
+        {
+            SetState(0);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!isPressed)
+        {
+            Press();
+            Coroutine = ReleaseAfterDelay(3f);
+            StartCoroutine(Coroutine);
+        }
+    }
+}
